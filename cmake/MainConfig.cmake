3532b86c156edf66065276cf7edc809151a055e3
lcrippa <crippa.lorenzo@gmail.com>
2025-07-07 16:28:16 +0200

    Update:
    Fixed a problem when long_path was selected false: the module file was
    incorrected copied as "module.in" into a directory called "default",
    where the hidden version.in file was also copied. This is incorrect
    because now "default" is the name of the module file, not of the
    directory the module file is in. Also, probably we shouldn't create the
    .version file in this case, which is a custom user installation.
    
    It is probably also not a good idea to set PATH_ID to "default" in this
    case. It is custom, not default.

--- cmake/MainConfig.cmake
+++ cmake/MainConfig.cmake
@@ -17,7 +17,7 @@ SET(DEFAULT_LOCATION "$ENV{HOME}/opt")
 #if LONG_PREFIX=on
 IF (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   IF (LONG_PREFIX)
-    SET(PATH_ID "${FC_ID}")
+    SET(PATH_ID "${FC_ID}" CACHE INTERNAL "Default Path ID" FORCE)
     #IF not master branch, include simplified branch name
     IF( (NOT GIT_BRANCH MATCHES "master") )
       SET(PATH_ID  "${PATH_ID}/${GIT_BRANCH}")
@@ -32,7 +32,7 @@ IF (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     SET(MODULE_NAME "${PROJECT_NAME}/${PATH_ID}/${VERSION}")
     SET(FULL_VER "${PATH_ID}/${VERSION}")
   ELSE()
-    SET(PATH_ID "default")
+    SET(PATH_ID "custom" CACHE INTERNAL "Custom Path ID" FORCE)
     #set to $HOME/opt/<libname>
     #set module name accordingly
     SET(DEFAULT_PREFIX "${DEFAULT_LOCATION}/${PROJECT_NAME}/${PATH_ID}")
@@ -40,7 +40,7 @@ IF (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     SET(FULL_VER "${PATH_ID}")
   ENDIF(LONG_PREFIX)
 ELSE()
-  SET(PATH_ID "default")
+  SET(PATH_ID "custom" CACHE INTERNAL "Custom Path ID" FORCE)
   #set to user defined CMAKE_INSTALL_PREFIX
   #set module name accordingly
   SET(DEFAULT_PREFIX "${CMAKE_INSTALL_PREFIX}")
