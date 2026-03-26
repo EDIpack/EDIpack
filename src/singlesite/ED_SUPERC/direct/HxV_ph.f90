b4aa9b12895101e01257e71763f813a46c56a9d5
Samuele Giuli <samuele.giuli@gmail.com>
2026-03-26 14:39:34 -0400

    corrected SUPERC/direct with phonons

--- src/singlesite/ED_SUPERC/direct/HxV_ph.f90
+++ src/singlesite/ED_SUPERC/direct/HxV_ph.f90
@@ -6,13 +6,13 @@
   if(A_ph/=0.d0)then
     if(iph < DimPh) then !bdg = sum_n |n+1> sqrt(n+1)<n|
         htmp = A_ph*sqrt(dble(iph))
-        i = i_el + (iph)*DimEl
+        j = i_el + (iph)*DimEl
         hv(j-MpiIshift) = hv(j-MpiIshift) + htmp*vin(i)
 
     endif
     if(iph > 1) then !bdg = sum_n |n+1> sqrt(n+1)<n|
         htmp = A_ph*sqrt(dble(iph-1))
-        i = i_el + (iph-2)*DimEl
+        j = i_el + (iph-2)*DimEl
         hv(j-MpiIshift) = hv(j-MpiIshift) + htmp*vin(i)
     endif
   end if
