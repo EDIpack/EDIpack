4a7a980cd8d3a251eb3b159b5f8f34888405a4b2
Samuele Giuli <samuele.giuli@gmail.com>
2026-04-14 15:02:55 -0400

    Revert "Phononic GF"
    This reverts commit 561c1c9b7e3a2eecc678fb96598606dee33ff271.

--- src/singlesite/ED_IO/get_dimp.f90
+++ src/singlesite/ED_IO/get_dimp.f90
@@ -1,8 +1,33 @@
-subroutine ed_get_Dimp_site(self,axis,z)
-    complex(8),dimension(:),intent(inout)       :: self ! Green's function matrix
-    character(len=*),optional                   :: axis ! Can be :f:var:`"m"` for Matsubara (default), :f:var:`"r"` for real
-    complex(8),dimension(:),optional            :: z    ! User provided array of complex frequency where to evaluate Self
-    character(len=1)                            :: axis_
-    complex(8),dimension(:),allocatable         :: z_
-    !TO BE COMPLETED
-end subroutine  ed_get_Dimp_site
+subroutine ed_get_dimp_site_n1(self,axis,z)
+#if __INTEL_COMPILER
+    use ED_INPUT_VARS, only: Nspin,Norb
+#endif
+  complex(8),dimension(:),intent(inout)       :: self ! phonon's Green's function matrix
+  character(len=*),optional                   :: axis ! Can be :f:var:`"m"` for Matsubara (default), :f:var:`"r"` for real
+  complex(8),dimension(:),optional            :: z    ! User provided array of complex frequency where to evaluate Self
+  character(len=1)                            :: axis_
+  complex(8),dimension(:),allocatable         :: z_
+  !
+  axis_='m';if(present(axis))axis_=trim(axis)
+  !
+  !
+  call allocate_grids
+  !
+  if(present(z))then
+     allocate(z_, source=z)
+  else
+     select case(axis_)
+     case default;stop "ed_get_dimp ERROR: axis is neither Matsubara, nor Realaxis"
+     case ('m','M')
+        z_ = dcmplx(0d0,wm)
+     case ('r','R')
+        z_ = dcmplx(wr,eps)
+     end select
+  endif
+  !
+  self = get_impD(z_,axis_)
+  call deallocate_grids
+  !
+end subroutine ed_get_dimp_site_n1
+
+
